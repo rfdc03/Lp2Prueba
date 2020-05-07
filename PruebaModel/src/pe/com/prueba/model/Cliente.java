6ccbe13cf7efb74f42105f83d9af5fda8968a6f6
Rodrigo Dulanto <>
2020-05-06 20:21:07 -0500

    Se crea la clase cliente.

---
+++ PruebaModel/src/pe/com/prueba/model/Cliente.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.com.prueba.model;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class Cliente {
+    private int dni;
+    private String nombre;
+
+    public Cliente(int dni, String nombre) {
+        this.dni = dni;
+        this.nombre = nombre;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
