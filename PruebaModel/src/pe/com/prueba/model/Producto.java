04e177b4ed1aebc10e5c05aedb2f5664050eaf65
Rodrigo Dulanto <>
2020-05-06 20:31:12 -0500

    Se crea la clase cliente.

---
+++ PruebaModel/src/pe/com/prueba/model/Producto.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.com.prueba.model;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class Producto {
+    private String nombre;
+    private float precio;
+}
